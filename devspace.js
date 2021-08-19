619307a78b1c062528189f82480d648348485a96
jimothyjohn <drunknmunky32@gmail.com>
2021-08-18 19:36:13 -0500

    Troubleshooting moves

--- devspace.js
+++ devspace.js
@@ -1,15 +1,14 @@
 const WebSocket = require('ws');
 
-/*
 // Comms
-const robotaddress = '192.168.1.30'
-const socketURL = 'ws://' + robotaddress + ':10000';
-robotsocket = new WebSocket(socketURL);
+const address = '192.168.1.30'
+const socketURL = 'ws://' + address + ':10000';
+const socket = new WebSocket(socketURL);
 
 
 // Format messge for transmission
 function SendMessage(message) {
-    robotsocket.send(message+'\0');
+    socket.send(message+'\0');
 }
 
 
@@ -19,21 +18,34 @@ function UpdateStatus() {
 }
 
 
-robotsocket.onmessage = function (event) { 
+socket.onmessage = function (event) { 
     // Parse robot message
     // Example message: [2007][PAUSED]
     const message = event.data.split('][');
     const code = parseInt(message[0].split('[')[1]);
     const response = message[1].split(']')[0];
     switch(code) {
-        // Determine error
+        // Home if not homed
+        case 1005:
+            console.log('Robot not activated! Activating...');
+            SendMessage('ActivateRobot');
+        case 1006:
+            console.log('Robot not homed! Homing...');
+            SendMessage('Home');
+        // Override error
+        case 1011:
+            console.log('Robot is in error! Clearing error...');        // Determine error
+            SendMessage('ResetError');
+            SendMessage('ResumeMotion');
         case 2007:
             const statuses = response.split(',');
             if(statuses[4]==1) {
-                console.log('Robot is paused');
+                console.log('Robot is pauseod');
+                SendMessage('ResumeMotion')
             }
             if(statuses[3]==1) {
                 console.log('Robot is in error');
+                SendMessage('ResetError')
             }
             break;
         // Receive coordinates 
@@ -44,159 +56,143 @@ robotsocket.onmessage = function (event) {
         // Ignore
         case 3001:
             process.exit(1);
-        // Override error
-        case 1011:
-            console.log('Robot is in error');
+
     }
     // Log response
     console.log(code, response);
 }
 
 
-robotsocket.on('open', function open() { UpdateStatus(); });
 
-
-// Format messge for transmission
-function SendMessage(socket, message) {
-    socket.send(message+'\0');
+// Read message from
+// Robot stuff
+class Pose {
+    constructor(
+        pose,
+        moveType='absolute',
+        poseType='pose',
+        workFrame='tool',
+        tool=[0,0,0,0,0,0],
+        work=[0,0,0,0,0,0]
+    ) {
+        this.pose = pose;
+        this.moveType = moveType;
+        if(moveType=='relative') { this.poseType = 'pose'; }
+        else { this.poseType = poseType; }
+        this.workFrame = workFrame;
+        this.tool = tool;
+        this.work = work;
+    }
 }
 
-
-function UpdateStatus(socket) { 
-    SendMessage(socket, 'GetStatusRobot');
-    SendMessage(socket, 'GetPose');
-}
 
 class Robot {
     constructor(
-        address='192.168.0.100',
         axes=6,
         workFrame='tool',
         tool=[0,0,0,0,0,0],
         work=[0,0,0,0,0,0]
     ) {
+        this.connected = false;
         this.axes = axes;
         this.workFrame = workFrame;
         this.tool = tool;
         this.work = work;
-        // May want to set default speed later
-        this.speed = 10;
-        const socketURL = 'ws://' + address + ':10000';
-        this.socket = new WebSocket(socketURL);
-        this.socket.onmessage = function (event) { 
-            // Parse robot message
-            // Example message: [2007][PAUSED]
-            const message = event.data.split('][');
-            const code = parseInt(message[0].split('[')[1]);
-            const response = message[1].split(']')[0];
-            switch(code) {
-                // Determine error
-                case 2007:
-                    const statuses = response.split(',');
-                    if(statuses[4]==1) {
-                        console.log('Robot is paused');
-                    }
-                    if(statuses[3]==1) {
-                        console.log('Robot is in error');
-                    }
+        this.home = new Pose([140,16,250,-180,0,0]); 
+    }
+
+    Move(pose) {
+        var moveCommand = '';
+        if(pose.moveType=='absolute') {
+            switch(pose.poseType) {
+                case 'joint':
+                    moveCommand = 'MoveJoints(';
                     break;
-                // Receive coordinates 
-                case 2027:
-                    const coords = response.split(',');
-                    this.pose = [parseFloat(coords[0]),parseFloat(coords[1]),parseFloat(coords[2]),parseFloat(coords[3]),parseFloat(coords[4]),parseFloat(coords[5])]
+                case 'pose':
+                    moveCommand = 'MovePose(';
                     break;
-                // Ignore
-                case 3001:
-                    process.exit(1);
-                // Override error
-                case 1011:
-                    console.log('Robot is in error');
+                case 'linear':
+                    moveCommand = 'MoveLin(';
+                    break;
             }
-            // Log response
-            console.log(code, response);
-        }
-        this.socket.on('open', function open() {
-            UpdateStatus(this.socket);
-            console.log('Connected!');
-        });
-    }
-}
-
-var mecademic = new Robot(address='192.168.1.30');
-*/
-
-class Robot {
-    constructor(
-        axes=6,
-        workFrame='tool',
-        tool=[0,0,0,0,0,0],
-        work=[0,0,0,0,0,0]
-    ) {
-        console.log('Hello!');
-        this.connected = false
-    }
-
-    // Format messge for transmission
-    SendMessage(message) {
-        this.socket.send(message+'\0');
-    }
-
-    UpdateStatus() { 
-        this.SendMessage('GetStatusRobot');
-        this.SendMessage('GetPose');
-    }
-
-    Connect(address) {
-        const socketURL = 'ws://' + address + ':10000';
-        this.socket = new WebSocket(socketURL);
-        this.socket.onmessage = function (event) { 
-            // Parse robot message
-            // Example message: [2007][PAUSED]
-            const message = event.data.split('][');
-            const code = parseInt(message[0].split('[')[1]);
-            const response = message[1].split(']')[0];
-            switch(code) {
-                // Determine error
-                case 2007:
-                    const statuses = response.split(',');
-                    if(statuses[4]==1) {
-                        console.log('Robot is paused');
-                    }
-                    if(statuses[3]==1) {
-                        console.log('Robot is in error');
-                    }
+        } else if(pose.moveType=='relative') {
+            switch(pose.workFrame) {
+                case 'tool':
+                    moveCommand = 'MoveLinRelTRF(';
                     break;
-                // Receive coordinates 
-                case 2027:
-                    const coords = response.split(',');
-                    const pose = [parseFloat(coords[0]),parseFloat(coords[1]),parseFloat(coords[2]),parseFloat(coords[3]),parseFloat(coords[4]),parseFloat(coords[5])]
+                case 'work':
+                    moveCommand = 'MoveLinRelWRF(';
                     break;
-                // Ignore
-                case 3001:
-                    process.exit(1);
-                // Override error
-                case 1011:
-                    console.log('Robot is in error');
             }
-            // Log response
-            console.log(code, response);
         }
-
-        this.socket.on('open', function open() {
-            console.log('Connected to robot at'+address);
-            this.connected = true;
-        });
+        SendMessage(moveCommand+pose.pose[0]+','+pose.pose[1]+','+pose.pose[2]+','+pose.pose[3]+','+pose.pose[4]+','+pose.pose[5]+')')
+        this.pose = pose;
     }
 
-    Test() {
-        console.log('In Test');
+    Home() {
+        this.Move(this.home);
     }
 
-    CallTest() {
-        this.Test();
+    Speed(percentage) {
+        this.speed = percentage;
+        SendMessage('SetCartAcc('+this.speed+')');
+        SendMessage('SetCartAngVel('+3*this.speed+')');
+        SendMessage('SetCartLinVel('+10*this.speed+')');
+        SendMessage('SetJointAcc('+1.5*this.speed+')');
+        SendMessage('SetJointVel('+this.speed+')');
+    }
+
+    Defaults() {
+        this.Speed(25);
+        SendMessage('SetBlending(0)');
+        SendMessage('SetTRF(0,0,0,0,0,0)');
+        SendMessage('SetWRF(0,0,0,0,0,0)');
+        this.tool=[0,0,0,0,0,0];
+        this.work=[0,0,0,0,0,0];
+        this.home = new Pose([140,16,250,-180,0,0]); 
     }
 }
 
 var mecademic = new Robot();
-mecademic.CallTest();
-mecademic.Connect('192.168.1.30');
+const startPose = new Pose([140,16,250,-180,0,0]);
+
+function shake(robot) {
+    robot.Speed(60);
+    robot.Loose();
+    SendMessage('SetTRF(0,0,-50,0,0,0)');
+    SendMessage('SetBlending(90)');
+    const wigglePose = new Pose([0,0,0,-5,0,0], moveType='relative');
+    robot.Move(startPose);
+    robot.Move(wigglePose);
+    wigglePose.pose[3] = 10;
+    for(i=0;i<6;i++) {
+        robot.Move(wigglePose);
+        wigglePose.pose[3] *= -1;
+    }
+    robot.Defaults();
+}
+
+
+function bounce(robot, dist=25, loops=2) {
+    robot.Speed(20);
+    SendMessage('SetBlending(10)');
+    const bouncePose = new Pose([0,0,dist,0,0,0], moveType='relative');
+    robot.Move(startPose);
+    for(i=0;i<loops;i++) {
+        robot.Move(bouncePose);
+        bouncePose.pose[2] *= -1;
+    }
+    robot.Defaults();
+}
+
+
+function dance(robot) {
+    const value = 0;
+}
+
+socket.on('open', function open() {
+    UpdateStatus();
+    console.log('Connected!');
+    shake(mecademic);
+    bounce(mecademic);
+});
